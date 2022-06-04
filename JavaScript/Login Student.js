80850f3396b2325a409f5733f46eec76316b969f
EhabSSarrawi <99269463+EhabSSarrawi@users.noreply.github.com>
2022-06-04 15:16:54 +0300

    Add files via upload

---
+++ JavaScript/Login Student.js
@@ -0,0 +1,45 @@
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
+
+
+function myFunction() {
+    let x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+
+const rmCheck = document.getElementById("Cremember"),
+    emailInput = document.getElementById("LogEmail");
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+    rmCheck.setAttribute("checked", "checked");
+    emailInput.value = localStorage.username;
+} else {
+    rmCheck.removeAttribute("checked");
+    emailInput.value = "";
+}
+
+function RememberMe() {
+    if (rmCheck.checked && emailInput.value !== "") {
+        localStorage.username = emailInput.value;
+        localStorage.checkbox = rmCheck.value;
+    } else {
+        localStorage.username = "";
+        localStorage.checkbox = "";
+    }
+}
+
